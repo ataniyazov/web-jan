bac0de7e71646d735c59673e733e23d9e0f1b232
Ahmadjan A <ataniyazov@outlook.com>
2020-08-15 07:29:10 -0400

    added another $ line

--- 18_Intro_To_jQuery/index.js
+++ 18_Intro_To_jQuery/index.js
@@ -9,3 +9,4 @@ $("div").css("background", "pink");
 $("div.highlight").css("width", "200px");
 $("div#third").css("border", "2px solid orange");
 $("div:first-of-type").css("color", "red");
+$("div").css("color", "steelblue")
